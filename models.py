69af11b69a4419027a9b91bda98acb99b5fd9108
SUSTC11612405 <897815338@qq.com>
2018-12-16 19:38:54 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import datetime
 from exts import db
 from flask_login import LoginManager, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -22,7 +23,35 @@ class User(db.Model, UserMixin):
 class Appoint(db.Model):
     __tablename__ = 'appointment'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    student_name = db.Column(db.String(50), nullable=False)
-    teacher_name = db.Column(db.String(50), nullable=False)
+    appointment_creat_time = db.Column(db.String(64), default=datetime.date.today())
+    prof_id = db.Column(db.Integer, db.ForeignKey('prof.prof_id'))
+    student_id = db.Column(db.Integer, db.ForeignKey('student.student_id'))
     start_time = db.Column(db.DATETIME, nullable=False)
     end_time = db.Column(db.DATETIME, nullable=False)
+
+
+class Student(db.Model):
+    __tablename__ = 'student'
+    student_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    student_name = db.Column(db.String(20), nullable=False)
+    student_sex = db.Column(db.String(2), nullable=False)
+    student_email = db.Column(db.String(30), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class Prof_info(db.Model):
+    __tablename__ = 'prof_info'
+    prof_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    prof_name = db.Column(db.String(20), nullable=False)
+    prof_pic = db.Column(db.String(128), nullable=False)
+    prof_title = db.Column(db.String(30), nullable=False)
+    prof_email = db.Column(db.String(30), nullable=False)
+    prof_interest = db.Column(db.String, nullable=False)
+    department_id = db.Column(db.Integer, db.ForeignKey('department.department_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class Department(db.Model):
+    __tablename__ = 'department'
+    department_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    department_name = db.Column(db.String(20), nullable=False)
